936209340f7982d3d593c25f5d65c607cda262e7
Kleber Mota <klebermo@gmail.com>
2014-06-01 14:13:04 -0300

    fixed form validation

--- src/main/webapp/WEB-INF/view/jsp/resources/extras/js/form_validation.js
+++ src/main/webapp/WEB-INF/view/jsp/resources/extras/js/form_validation.js
@@ -1,32 +1,106 @@
+function parse(regexString){
+    var regex = /((?!\[|\{).(?!\]|\}))|(?:\[([^\]]+)\]\{(\d+)\})/g,
+        match,
+        model = [];
+    while (match = regex.exec(regexString)) {
+        if(typeof match[1] == 'undefined'){
+            for(var i=0;i<match[3];i++){
+                model.push(match[2]);
+            }
+        }else{
+            model.push(match[1]);
+        }
+    }
+    return model;
+}
+
+function replaceAt(s, n, t) {
+    return s.substring(0, n) + t + s.substring(n + 1);
+}
+
 function size_of(regex) {
-	//
+	var parsedRegexp = parse(regex);
+	return parsedRegexp.length;
 }
 
 function type_of(regex, posicao) {
-	//
+	var parsedRegexp = parse(regex);
+	var pos = parsedRegexp[posicao];
+	
+	if(pos == '0-9')
+		return 'number';
+	
+	if(pos == 'A-Z' || pos == 'a-z')
+		return 'string';
+	
+	return pos;
 }
 
-function generate_string(tamanho) {
-	//
+function generate_string(regex, tamanho) {
+	var str = '';
+	
+	for(var i=0; i<tamanho; i++) {
+		var type = type_of(regex, i);
+		if(type == 'number' || type == 'string')
+			str = str + '_';
+		else
+			str = str + type;
+	}
+	
+	return str;
 }
 
-$(document).on('.valida', 'focus', function(){
-	var regex = $(this).attr('pattern');
-	
-	var counter = 0;
-	var tam = size_of(regex);
-	var str = generate_string(tam);
-	
-	$(this).val(str);
-	
-	$(this).keypress(function(event){
-		var tecla = e.which;
-		
-		if(typeof tecla == type_of(regex, counter)){
-			str = str + tecla;
-			counter++;
-		}
-		
-		$(this).val(str);
-	});
+var counter;
+var tam;
+var str;
+var regex;
+
+$('.valida').each(function(){
+    
+    $(this).on('focus', function(e){
+        regex = $(this).attr('pattern');
+        
+        counter = 0;
+        tam = size_of(regex);
+        str = generate_string(regex, tam);
+        
+        $(this).val(str);
+    });
+    
+    $(this).on('keypress', function(e){
+        e.preventDefault();
+        
+        var tecla = e.which;
+        
+        if(tecla >= 48 && tecla <= 57)
+            var tecla2 = tecla - 48;
+        else
+            var tecla2 = String.fromCharCode(tecla);
+        
+        result = $("<div>");
+        result.append( "tecla = "+tecla+"<br>" );
+        
+        var t = type_of(regex, counter);
+        
+        if(counter < tam) {
+            if(t != 'number' && t != 'string') {
+                str = replaceAt(str, counter, t);
+                counter++;
+            }
+            
+            t = type_of(regex, counter);
+            
+            if(typeof tecla2 == t) {
+                result.append( "tecla2 = "+tecla2+"<br>" );
+                str = replaceAt(str, counter, tecla2);
+                counter++;
+            }
+        }
+        
+        result.append( "counter = "+counter+"<br>" );
+        $("#result").empty().append(result);
+        
+        $(this).val(str);
+    });
+    
 });
